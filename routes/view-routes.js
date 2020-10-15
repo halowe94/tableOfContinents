6d0b3f3b589c86c380b907103113fc6b48b23c66
H Lowe <haleylowe0@gmail.com>
2020-10-15 14:03:10 -0700

    put ajax call within function

--- routes/view-routes.js
+++ routes/view-routes.js
@@ -5,13 +5,15 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
     res.render("blog-simple");
   });
+  
   app.get('/africa', function(req, res){
     //ajax request to the backend and in the .then execute the res.render with the response object you got back
-    let continentResults = [
-      $.ajax({
+    let continentResults = function () {
+      return $.ajax({
         url:'api/continents',
         method: "GET",
       })
+    }
 
         // {
         // names: "Africa", 
@@ -19,11 +21,10 @@ module.exports = function (app) {
         // pop: "1287920000", 
         // number_countries: "54", 
         // largest_country:"Nigeria",
-        // popular_dish: "Tajine"}
-    ];
-        
-    res.render('africa', continentResults[0]);
+        // popular_dish: "Tajine"}        
+    .then(res.render('africa', continentResults[0]));
 });
+
   app.get("/asia", function (req, res) {
     res.render("asia");
   });
