58b34848e47185eff49e545839b9f214861d8173
H Lowe <haleylowe0@gmail.com>
2020-10-17 19:47:52 -0700

    changed the syntax of the submitBtn.js

--- public/assets/js/submitBtn.js
+++ public/assets/js/submitBtn.js
@@ -1,18 +1,32 @@
+let $ = require("jquery");
+// let submitBtn = document.getElementById("submitBtn");
+// let emailInput = document.getElementById("emailInput").value;
+
+// var submitEmail = function (emailInput) {
+//     return $.ajax({
+//       url: "/api/emails",
+//       data: emails,
+//       method: "POST",
+//     });
+//   };
+
+// submitFunc(){
+//     console.log(emailInput);
+//     submitBtn.addEventListener("click", submitEmail);
+// };
+
+// submitFunc();
+
 let submitBtn = document.getElementById("submitBtn");
-let emailInput = document.getElementById("emailInput");
 
-var submitEmail = function (emailInput) {
+const submitEmail =  function(event){
+    console.log("hi it works")
+    event.preventDefault()
     return $.ajax({
-      url: "/api/emails",
-      data: emails,
-      method: "POST",
+        url: "/api/continents/emails",
+        data: document.getElementById("emailInput").value,
+        method: "POST",
     });
-  };
+ };
 
-submitFunc(){
-    console.log(emailInput);
-    submitBtn.addEventListener("click", submitEmail);
-};
-
-submitFunc();
-
+submitBtn.addEventListener("submit", submitEmail);
