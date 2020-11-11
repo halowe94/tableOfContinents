aa204d98d02a05016232875e9a7d4488f31446e3
halowe <haleylowe0@gmail.com>
2020-11-10 21:54:53 -0800

    updating email route

--- public/assets/js/continents.js
+++ public/assets/js/continents.js
@@ -18,18 +18,18 @@
 // submitFunc();
 
 let submitBtn = document.getElementById("form");
-console.log('submitBtn page');
-console.log(submitBtn);
-console.log(document.getElementById("emailInput").value);
+// console.log("submitBtn page");
+// console.log(submitBtn);
+// console.log(document.getElementById("emailInput").value);
 
-const submitEmail =  function(event){
-    console.log("hi it works")
-    event.preventDefault()
-    return $.ajax({
-        url: '/api/continents/emails/',
-        data: document.getElementById("emailInput").value,
-        method: "POST",
-    });
- };
+const submitEmail = function (event) {
+  //   console.log("hi it works");
+  event.preventDefault();
+  $.ajax({
+    url: "/api/continents/emails",
+    data: document.getElementById("emailInput").value,
+    method: "POST",
+  });
+};
 
-submitBtn.addEventListener("click", submitEmail);
+submitBtn.addEventListener("submit", submitEmail);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,45 +1,49 @@
-var db = require('../models');
+var db = require("../models");
 
 module.exports = function (app) {
-    app.post('/api/continents', function (req, res) {
-        console.log('here2');
-        db.continent.create({
-            names: req.body.names,
-            area_km: req.body.area_km,
-            pop: req.body.pop,
-            number_countries: req.body.number_countries,
-            largest_country: req.body.largest_country,
-            popular_dish: req.body.popular_dish
-        }).then(function (data) {
-            res.json(data)
-        })
-    }
-    )
+  app.post("/api/continents", function (req, res) {
+    console.log("here2");
+    db.continent
+      .create({
+        names: req.body.names,
+        area_km: req.body.area_km,
+        pop: req.body.pop,
+        number_countries: req.body.number_countries,
+        largest_country: req.body.largest_country,
+        popular_dish: req.body.popular_dish,
+      })
+      .then(function (data) {
+        res.json(data);
+      });
+  });
 
-    app.get('/api/continents/:names', function (req, res) {
-        console.log(req);
-        db.continent.findOne({
-            where: {
-                names: req.params.names
-            }
-        }).then(function (results) {
-            res.json(results);
-        })
-    });
+  app.get("/api/continents/:names", function (req, res) {
+    console.log(req);
+    db.continent
+      .findOne({
+        where: {
+          names: req.params.names,
+        },
+      })
+      .then(function (results) {
+        res.json(results);
+      });
+  });
 
-    app.post('/api/continents/emails/', function (req, res) {
-        //refers to table
-        console.log(req.body);
-        console.log("req.body: ", req.body);
-        db.emails.create({
-            //refers to column in table
-            email_address: req.body
-        }).then(function (results) {
-            console.log("email is added to the db");
-            res.json(results);
-        })
-    })
-
-}
+  app.post("/api/continents/emails", function (req, res) {
+    //refers to table
+    console.log(req.body);
+    console.log("req.body: ", req.body);
+    db.emails
+      .create({
+        //refers to column in table
+        email_address: req.body,
+      })
+      .then(function (results) {
+        console.log("email is added to the db");
+        res.json(results);
+      });
+  });
+};
 
 //front end is responsible for creating an ajax request with key name email and the value set to the text inside of the newsletter input
