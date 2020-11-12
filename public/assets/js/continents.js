b023312a78714b60b408e8b3439b65252fb72e61
H Lowe <haleylowe0@gmail.com>
2020-11-12 13:15:14 -0800

    submit action added to form id

--- public/assets/js/continents.js
+++ public/assets/js/continents.js
@@ -1,22 +1,3 @@
-//let $ = require("jquery");
-// let submitBtn = document.getElementById("submitBtn");
-// let emailInput = document.getElementById("emailInput").value;
-
-// var submitEmail = function (emailInput) {
-//     return $.ajax({
-//       url: "/api/emails",
-//       data: emails,
-//       method: "POST",
-//     });
-//   };
-
-// submitFunc(){
-//     console.log(emailInput);
-//     submitBtn.addEventListener("click", submitEmail);
-// };
-
-// submitFunc();
-
 let submitBtn = document.getElementById("form");
 // console.log("submitBtn page");
 // console.log(submitBtn);
